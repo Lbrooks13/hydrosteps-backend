856c0d2eba34cde3871b3378fc9b5ab472212b1c
Lbrooks13 <lbrooks1308@gmail.com>
2025-05-12 19:20:37 +0100

    Disabled CSFR on register API endpoint

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,4 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Entry
@@ -27,6 +28,7 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.decorators import api_view, permission_classes
 
+@csrf_exempt
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def register_user(request):
